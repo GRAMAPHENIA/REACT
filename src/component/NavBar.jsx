11347b29078ef938a64115502d0dc64d7eac1863
gramaphenia <dicoratojuanpablo@gmail.com>
2023-10-16 11:51:30 -0300

    fix

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,29 +1,14 @@
-import React from 'react'
+import React from "react";
 import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <nav className="my-10 text-3xl space-x-4 flex items-center justify-center">
-       <Link
-      href="/"
-      className="underline decoration-amber-100 underline-offset-4"
-    >
-      Inicio
-    </Link>
-    <Link
-      href="/datos"
-      className="underline decoration-amber-100 underline-offset-4"
-    >
-      Teoria
-    </Link>
-    <Link
-      href="/tasks"
-      className="underline decoration-amber-100 underline-offset-4"
-    >
-      Aplicación
-    </Link>
-  </nav>
-  )
-}
+    <nav className="navbar flex items-center justify-center">
+      <Link href="/">Inicio</Link>
+      <Link href="/datos">Teoria</Link>
+      <Link href="/tasks">Aplicación</Link>
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
