ccb4df046eab4a07c9878ae4edfa2bb9b8145d0d
gramaphenia <dicoratojuanpablo@gmail.com>
2023-10-16 15:25:07 -0300

    act

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-start">
-      <section className="max-w-[400px] sm:max-w-[720px]">
+    <main className="flex flex-col items-center justify-between">
+      <section className="flex flex-col justify-center items-center max-w-[400px] h-[640px] sm:max-w-[720px]">
         <h1 className="text-6xl text-center">React</h1>
         <h1 className="pb-10 text-4xl text-center">
           <span>[</span>hooks<span>]</span>
